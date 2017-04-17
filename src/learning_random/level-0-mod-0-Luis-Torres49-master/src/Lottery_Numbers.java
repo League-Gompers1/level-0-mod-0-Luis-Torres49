8a6e258eaed44fe13e0ac92101b524cdf384bca8
Luis-Torres49 <luis.medatorres@gompersstudent.org>
2017-04-17 14:57:28 -0700

    Add files via upload

---
+++ src/learning_random/level-0-mod-0-Luis-Torres49-master/src/Lottery_Numbers.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Lottery_Numbers {
+
+	public static void main(String[] args) {
+		for (int i =0 ; i<6 ; i++){
+		
+		
+		
+		
+		int lotteryNumber = new Random().nextInt(99);
+		System.out.println(lotteryNumber);}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/learning_random/level-0-mod-0-Luis-Torres49-master/src/robot/Extra.java
@@ -0,0 +1,23 @@
+package robot;
+import org.jointheleague.graphical.robot.Robot;
+
+public class Extra { 
+	public static void main(String[] args) throws Exception{
+Robot LT = new Robot("mini");
+LT.penDown();
+	LT.move(200);
+	LT.turn(90);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
